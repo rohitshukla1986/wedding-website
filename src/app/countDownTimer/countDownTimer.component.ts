06cb541eabd82dd8ba215a202cf7f037c89b79b4
Rohit Shukla <rohit.shukla@blackpepper.co.uk>
2019-11-13 15:09:47 +0000

    First commit

---
+++ src/app/countDownTimer/countDownTimer.component.ts
@@ -0,0 +1,36 @@
+import {Component, OnInit} from '@angular/core';
+import {interval, timer} from 'rxjs';
+
+@Component({
+  selector: 'app-count-down-timer',
+  templateUrl: './countDownTimer.component.html',
+  styleUrls: ['./countDownTimer.component.css']
+})
+export class CountDownTimerComponent implements OnInit {
+
+  private countDownDate: number;
+  public daysLeft: number;
+  public hoursLeft: number;
+  public minutesLeft: number;
+  public secondsLeft: number;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.countDownDate = new Date(2020, 9, 26, 0, 0, 0).getTime();
+    timer(0, 1000).subscribe(
+      () => {
+        this.recalculateDaysLeft();
+      });
+  }
+
+  recalculateDaysLeft(): void {
+    const distance = this.countDownDate - new Date().getTime();
+
+    this.daysLeft = Math.floor(distance / (1000 * 60 * 60 * 24));
+    this.hoursLeft = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    this.minutesLeft = Math.floor(distance % ((1000 * 60 * 60)) / (1000 * 60));
+    this.secondsLeft = Math.floor((distance % (1000 * 60)) / 1000);
+  }
+}
