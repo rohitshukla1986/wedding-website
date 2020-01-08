f00dae31bbac273c26866b518f3520111bf4cdb2
Rohit Shukla <rohit.shukla@blackpepper.co.uk>
2020-01-08 15:49:39 +0000

    on scroll menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,12 +1,21 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+  public topOfPage = true;
 
   constructor() {
+  }
+
+  ngOnInit(): void {
+    window.onscroll = () => this.onScroll();
+  }
+
+  onScroll() {
+    this.topOfPage = !(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80);
   }
 }
